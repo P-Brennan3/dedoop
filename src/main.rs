d25b9dd1a44185a8e9da1b28cacd4c2ec8b0656c
p-brennan3 <p-brennan3>
2024-11-25 09:51:44 -0500

    finds nested files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::io;
 use std::error::Error;
 use std::fs::{self,File};
@@ -26,43 +26,51 @@ fn main() -> Result<(), Box<dyn Error>> {
   // we need a map of the file size to the files that have it
   let mut size_files_map: HashMap<u64, Vec<String>> = HashMap::new();
 
-  let files = fs::read_dir(dir)?;
+  let mut dirs_to_visit: VecDeque<String> = VecDeque::from([dir]);
 
-  // 2. for each file:
-  for entry in files {
-      let entry = entry?;
-      let path = entry.path();
-      let path_str = path.to_str().expect("Error");
+  // continue looping while we have directories to visit
+  while !dirs_to_visit.is_empty() {
+    let dir = dirs_to_visit.pop_front().expect("We should always have a value here");
+    println!("Searching {dir}...");
+    let files = fs::read_dir(dir)?;
+    // 2. for each file:
+    for entry in files {
+        let entry = entry?;
+        let path = entry.path();
+        let path_str = path.to_str().expect("Error").to_string();
 
-      // ignore directories for now
-      if path.is_dir() {
-        continue;
-      }
+        // add the directory to the stack
+        if path.is_dir() {
+          dirs_to_visit.push_front(path_str.clone());
+        }
 
-      // insert this file size into the map
-      let file = File::open(&path)?;
-      let size = file.metadata()?.size();
-      size_files_map.entry(size).or_insert_with(Vec::new).push(String::from(path_str));
+        // insert this file size into the map
+        let file = File::open(&path)?;
+        let size = file.metadata()?.size();
+        size_files_map.entry(size).or_insert_with(Vec::new).push(path_str);
+    }
+
   }
-
 
   for (size, files) in &size_files_map {
     let files_count = files.len();
-    println!("Comparing {files_count} file(s) of size {size}");
-      
-    // we need a map of the file hash to the files that have it
     let mut sha_files_map: HashMap<String, Vec<String>> = HashMap::new();
-    for file in files {
-        match hash_file(&file) {
-        // if we have a hash returned and not an error
-        // 1. get the entry in the HashMap for this hash
-        // 2. if it doesnt exist, make the value a new emtpy Vector
-        // 3. either way push into the vector the path for the file
-        Ok(hash) => {
-          sha_files_map.entry(hash).or_insert_with(Vec::new).push(String::from(file))
-        },
-        Err(_) => ()
-      } 
+    if files_count > 1 {
+      println!("Comparing {files_count} file(s) of size {size}");
+        
+      // we need a map of the file hash to the files that have it
+        for file in files {
+            match hash_file(&file) {
+            // if we have a hash returned and not an error
+            // 1. get the entry in the HashMap for this hash
+            // 2. if it doesnt exist, make the value a new emtpy Vector
+            // 3. either way push into the vector the path for the file
+            Ok(hash) => {
+              sha_files_map.entry(hash).or_insert_with(Vec::new).push(String::from(file))
+            },
+            Err(_) => ()
+          } 
+        }
     }
     for (_, files) in &sha_files_map {
       if files.len() > 1 {
